a0a0a6c19adaaef9773727aefcd7d451f78dc7a3
James_McCauley <James_McCauley@student.uml.edu>
2009-04-01 18:06:07 +0000

    Here, this should finally work.

---
+++ JMMusicProject/JMMusic/src/SoundPack/JMAudioFunctions.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package SoundPack;
+
+import java.io.File;
+
+/**
+ *
+ * @author James
+ */
+public class JMAudioFunctions {
+
+}
