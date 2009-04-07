ce8efb11520e92eee9310cb6bd62080a569218dd
jebert5461 <jebert5461@gmail.com>
2009-04-07 17:09:55 +0000

    Added functionality.

--- ETMusic/src/GUI/ETTableModel.java
+++ ETMusic/src/GUI/ETTableModel.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package soundlibrary;
+package GUI;
 
 import java.util.Vector;
 import javax.swing.table.DefaultTableModel;
