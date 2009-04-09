b652d29487040294b3726e03b0559f3356fc4468
James_McCauley@student.uml.edu <James_McCauley@student.uml.edu@59fbd666-1933-11de-9433-ad2e3fad96c5>
2009-04-09 13:18:18 +0000

    Here are my project files.

--- trunk/JMMusicProject/JMMusic/src/SoundPack/JMAudioFunctions.java
+++ trunk/JMMusicProject/JMMusic/src/SoundPack/JMAudioFunctions.java
@@ -6,6 +6,8 @@
 package SoundPack;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 /**
  *
@@ -13,4 +15,42 @@ import java.io.File;
  */
 public class JMAudioFunctions {
 
+      static int INTERFERENCE = 500;
+      static int RANDOMBYTE = 5;
+
+      public static void main(String[] arguments) {
+      }
+
+      public void saveFile(String s){
+           try {
+             File song = new File(s);
+             FileInputStream file = new FileInputStream(song);
+             File trashedSong = new File("C:\\trashed.mp3");
+             FileOutputStream trash = new FileOutputStream(trashedSong);
+             boolean eof = false;
+//             int count = 0;
+             System.out.println("Creating file ...");
+             while (!eof) {
+                 int input = file.read();
+                 if (input == -1)
+                     eof = true;
+//                 else {
+//                     count++;
+//                     if (count % INTERFERENCE == 0) {
+//                         int newInput = input + RANDOMBYTE;
+//                         if (newInput > 255)
+//                             newInput = newInput - 255;
+//                         trash.write(newInput);
+//                     }
+                 else{
+                         trash.write(input);
+                 }
+             }
+             file.close();
+             trash.close();
+             System.out.println("Done");
+         } catch (Exception e) {
+             System.out.println("Error — " + e.toString());
+         }
+      }
 }
